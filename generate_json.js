15689b7207a82272658c0089cbc6fbf3de0bb800
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-04-02 10:48:37 +0200

    fix: corrected results.json to include links instead of full images

--- generate_json.js
+++ generate_json.js
@@ -6,27 +6,27 @@ import { join } from "path";
 
 let results = [];
 
-let image_input;
-let svg_data;
-let im;
-let imageOfStructuresvg;
+//let image_input;
+//let svg_data;
+let fileNameInputImage;
+let fileNameSvg;
 let contentSdf;
 let page;
 let fileNameSdfFile;
 let pageFile;
 for (let index = 0; index < 1000; index++) {
-  im = "input/images/struct" + index + ".png";// only this numbering starts at 0 - the others at 1
-  if (existsSync(im)) {
-    image_input = readFileSync(im);
-    imageOfStructuresvg = "input/separated_svg/str" + (index + 1) + ".svg";
-    svg_data = readFileSync(imageOfStructuresvg).toString();
+  fileNameInputImage = "input/images/struct" + index + ".png";// only this numbering starts at 0 - the others at 1
+  if (existsSync(fileNameInputImage)) {
+   // image_input = readFileSync(im);
+    fileNameSvg = "input/separated_svg/str" + (index + 1) + ".svg";
+    //svg_data = readFileSync(imageOfStructuresvg).toString();
     fileNameSdfFile = "input/separated_sdf/str" + (index + 1) + ".sdf";
     contentSdf = readFileSync(fileNameSdfFile).toString();
     pageFile = "input/page-number/in" + (index + 1) + ".txt";
     page = readFileSync(pageFile).toString();
     results.push({
-      inPngImage: image_input,
-      outSvgImage: svg_data,
+      fileNameInputImage: fileNameInputImage,
+      fileNameSvg: fileNameSvg,
       sdf: contentSdf,
       pageNumber: page
     });
